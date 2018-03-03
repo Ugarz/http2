14ebd626fa803dcf3170ebe51f0fd2d1869e1d01
Ugo Arzur <ugo.arzur@gmail.com>
2018-03-03 13:52:13 +0100

    add scripts cert generator

---
+++ scripts/generate_cert.sh
@@ -0,0 +1,11 @@
+cd config
+openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
+echo "server.pass.key created"
+openssl rsa -passin pass:x -in server.pass.key -out server.key
+echo "server.key created"
+rm server.pass.key
+echo "server.pass.key removed"
+openssl req -new -key server.key -out server.csr
+echo "server.csr created"
+openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
+echo "server.crt certificate created"
